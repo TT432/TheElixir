21eb9b375887f43ef60d594693586d341552321b
TT432 <2437315224@qq.com>
2021-07-10 23:34:01 +0800

    添加 “森罗百草枯”

--- src/main/java/org/teacon/theelixir/RegistryHandler.java
+++ src/main/java/org/teacon/theelixir/RegistryHandler.java
@@ -6,6 +6,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.teacon.theelixir.item.ElixirItem;
 import org.teacon.theelixir.item.Restrainer;
+import org.teacon.theelixir.item.SunroVientianeParaquatItem;
 import org.teacon.theelixir.item.TextItem;
 
 /**
@@ -16,11 +17,13 @@ public class RegistryHandler {
     public static final ElixirItem ELIXIR_ITEM = new ElixirItem();
     public static final TextItem TEXT_ITEM = new TextItem();
     public static final Restrainer RESTRAINER = new Restrainer();
+    public static final SunroVientianeParaquatItem SUNRO_VIENTIANE_PARAQUAT_ITEM = new SunroVientianeParaquatItem();
 
     @SubscribeEvent
     public static void onItemRegistry(RegistryEvent.Register<Item> event) {
         event.getRegistry().register(ELIXIR_ITEM);
         event.getRegistry().register(TEXT_ITEM);
         event.getRegistry().register(RESTRAINER);
+        event.getRegistry().register(SUNRO_VIENTIANE_PARAQUAT_ITEM);
     }
 }
